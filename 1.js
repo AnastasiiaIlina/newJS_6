cf41275fab058c3045ca669e8aef4e02b84a5b0d
Anastasiia <itma2011@yandex.ua>
2018-03-12 23:30:53 +0200

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,32 +1,93 @@
-let keyTrainer = {};
-keyTrainer.chars = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+// task 1-3
 
-// task 2-3
-keyTrainer.charCount = '';
-keyTrainer.setCharCount = () =>  {
-	let enterNumber = +prompt('Введите к-тво символов, которoе нужно набрать');
+let keyTrainer = {
+	chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
 
-	const checkPositiveInteger =()=> {
+	charCount:'',
 
-		while(!(Number.isInteger(enterNumber) && enterNumber>1)) {
-			enterNumber = +prompt('Неверный ввод. Попробуйте еще раз');
-		}	
-	}
+	setCharCount:function() {
+		let enterNumber = +prompt('Введите к-тво символов, которoе нужно набрать');
 
-	checkPositiveInteger();
-}
+		document.querySelector('.enterNumber').innerHTML = enterNumber + ' символа (-ов).';
+		this.charCount=enterNumber;
+
+			
+
+		const checkPositiveInteger =()=> {
+
+			while(!(Number.isInteger(enterNumber) && enterNumber>0)) {
+				enterNumber = +prompt('Неверный ввод. Попробуйте еще раз');
+			}	
+		}
+
+		checkPositiveInteger();
+
+		return enterNumber;
+
+	},
+
+
+}; /*end keyTrainer*/
+
+
+
 
 // task 4
-	
-keyTrainer.task = '';
-keyTrainer.createTask = {};
+keyTrainer.task=[];
+keyTrainer.createTask =function() {
+	keyTrainer.task.lenght = this.setCharCount();
+
+	for ( i=0; i<keyTrainer.task.lenght; i++) {
+		 keyTrainer.task[i] = this.chars[Math.floor(Math.random()*keyTrainer.task.lenght)];
+		 document.querySelector('.out').innerHTML = keyTrainer.task + ',' ;
+		
+	}
+
+		console.log(keyTrainer.task);
+}
+
+
+// task 5
+keyTrainer.userInput = '';
+keyTrainer.startTask = function() {
+		let result = prompt(`Наберите указанные символы: ${keyTrainer.task}`);
+
+
+		keyTrainer.userInput = result;
+
+		if(keyTrainer.userInput === keyTrainer.task.join('')) {
+			console.log('Поздравдляем! Вы справились без ошибок!');
+		}
+
+		else {
+			keyTrainer
+
+			console.log('fyyyy');
+
+		}
+
+
+		// return result;
+}
+
+
+
+
+
+
+
+
+
 
 
 
 
 function run(){
-	keyTrainer.setCharCount();
-	// keyTrainer.checkPositiveInteger();
+	// keyTrainer.setCharCount();
+
+// keyTrainer.checkPositiveInteger();
+		keyTrainer.createTask(); 
+		keyTrainer.startTask();
 }
 
 run();
